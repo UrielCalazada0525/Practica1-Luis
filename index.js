b7c21d77854344f909ce0beaf610fce018cb2d10
UrielCalazada0525 <89798086+UrielCalazada0525@users.noreply.github.com>
2021-09-06 21:45:24 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+function getIMC(){
+  var peso=parseFloat(document.getElementById('peso').value);
+  var altura=parseFloat(document.getElementById('altura').value);
+  var imc=peso/(altura*altura);
+  document.getElementById("imc").innerHTML=imc;
+}
