67dfb18a41fa0d51b32688b6e68c61f9063536e7
UrielCalazada0525 <89798086+UrielCalazada0525@users.noreply.github.com>
2021-09-13 11:24:29 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,4 +3,12 @@ function getIMC(){
   var altura=parseFloat(document.getElementById('altura').value);
   var imc=peso/(altura*altura);
   document.getElementById("imc").innerHTML=imc;
+  if (imc<=18.50){
+    resultado="Es bajo peso";}
+    else
+     if (imc>=16.00 && imc<=16.99){
+     resultado="Delgadez severa";}
+    else
+    if (imc>=17.00 && imc<=18.49){
+     resultado="Delgadez severa";} 
 }
