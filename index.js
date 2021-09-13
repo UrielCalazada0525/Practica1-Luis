1c1b771e7f56be31ed04f1541396245260930b6d
UrielCalazada0525 <89798086+UrielCalazada0525@users.noreply.github.com>
2021-09-13 11:52:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -11,4 +11,19 @@ function getIMC(){
     else
     if (imc>=17.00 && imc<=18.49){
      resultado="Delgadez severa";} 
+  if (imc>=17.00 && imc<=18.49){resultado="Delgadez aceptable";}
+    else
+    if (imc>=18.58 && imc<=24.99){resultado="Normal";}
+    else
+    if (imc<=25.00){resultado="Sobre peso";}
+    else
+    if (imc>=25.00 && imc<=29.99){resultado="Pre-obeso (riesgo)";}
+    else
+    if (imc>=30.00){resultado="obeso";}
+      else
+      if (imc>=30.00 && imc<=34.99){resultado="obeso tipo I (riesgo moderado)";}
+      else
+      if (imc>=35.00 && imc<=39.99){resultado="obeso tipo II (riesgo severo)";}
+      if (imc>=40.00){resultado="obeso tipo III (riesgo muy severo)";}
+      alert(resultado); 
 }
